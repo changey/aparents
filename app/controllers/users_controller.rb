e8272dfe331e65fdcc50e6abefb9d93922bbe559
Eric Chang <changey@Erics-MacBook-Pro.local>
2013-05-22 16:24:32 -0400

    sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,28 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+
+    if @user.save
+      flash[:success] = "Welcome to the Netwoek Stickies App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  #the method to sign in a user
+  def signed_in_user
+    redirect_to signin_path, notice: "Please sign in." unless signed_in?
+  end
+
+  #the method to check if the user is the authorized user to view a note
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
 end
