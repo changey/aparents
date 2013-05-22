7755167337627d2b2546e061d05430dc096a9a62
Eric Chang <changey@Erics-MacBook-Pro.local>
2013-05-22 17:02:55 -0400

    signout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
+  
+  before_save :create_remember_token
+  
+  private
+
+  #the method to create a unique token to be remembered for users to see his/her account without log in every time
+  def create_remember_token
+    self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20130522202948) do
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
